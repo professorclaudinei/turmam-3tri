3b265181f89acd7ef2b934af016f350e4809d00d
Professor Claudinei MD <107876477+professorclaudinei@users.noreply.github.com>
2022-11-24 12:35:13 -0300

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ hamburger.addEventListener('click', () => {
 document.addEventListener('scroll', () => {
 	var scroll_position = window.scrollY;
 	if (scroll_position > 250) {
-		header.style.backgroundColor = '#807f00';
+		header.style.backgroundColor = '#fbff03';
 	} else {
 		header.style.backgroundColor = 'transparent';
 	}
